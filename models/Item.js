9896d4af4f633830a855dfaeda0c1e23dba0d101
panji97 <panjisetyokurniawan@gmail.com>
2022-07-24 01:51:23 +0700

    deploy heroku

---
+++ models/Item.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const itemsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    def: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    default: "per night",
+  },
+  sumBooking: {
+    type: Number,
+    default: 0,
+  },
+  categoryId: {
+    type: ObjectId,
+    ref: "Category",
+  },
+
+  imageId: [
+    {
+      type: ObjectId,
+      ref: "Image",
+    },
+  ],
+  featureId: [
+    {
+      type: ObjectId,
+      ref: "Feature",
+    },
+  ],
+  activityId: [
+    {
+      type: ObjectId,
+      ref: "Activity",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Item", itemsSchema);
